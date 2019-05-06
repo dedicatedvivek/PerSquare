cff65cd124e6d9496686bb61e327920e49a468db
dedicatedvivek <vivekiyer98@gmail.com>
2019-05-06 11:27:08 +0530

    initialized

---
+++ resources/views/loginpage.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Login</title>
+
+	<style type="text/css">
+
+	body{
+		padding: 20%;
+	}
+		.outer-container{
+			background-color: #6a6e75;
+			max-width: 30%;
+
+		}
+
+		.logo-circle{
+			border-radius: 50%;
+			background-color: #73cec8;
+			width: 15%;
+		}
+		.logo-box{
+			height: 50%;
+			width: 50%;
+			background-color: #23a89f;
+		}
+	</style>
+</head>
+<body>
+
+<center>
+
+	<div class="outer-container">
+hi
+		<div class="logo-circle">
+			hi
+			<div class="logo-box">
+				hi
+				
+		</div>
+
+
+	</div>
+	</center>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+      <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
+    <title>Flats</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="section">
+
+            <div class="row">
+                <div class="col s12 center">
+                    <h3><i class="mdi-content-send brown-text"></i></h3>
+                    <h4>Flats</h4>
+                  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="https://cdnspimgsulekhalive.azureedge.net/cdn/images/property/project/detail/-projectimagelogo_2018091705135162318.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+  </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+</html>
